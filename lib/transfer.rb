9905a02595556932198136e00de5b6474ad9b767
Romano Livi <romano_livi@outlook.com>
2020-12-21 19:03:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   def execute_transaction
     if @sender.balance < @amount
       @status = "rejected"
-      "Transaction rejected. Please check your account balance."
+      return "Transaction rejected. Please check your account balance."
       
     elsif @status == "completed"
       puts "Transaction was already executed"

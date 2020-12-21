7d8b5e1ff7bf521ede66a2680070e60eee554ef2
Romano Livi <romano_livi@outlook.com>
2020-12-21 19:01:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,7 +26,6 @@ class Transfer
       @sender.deposit(@amount * -1)
       @receiver.deposit(@amount)
       @status = "complete"
-      puts "Successful transaction of $#{@amount}."
     end
   end
   

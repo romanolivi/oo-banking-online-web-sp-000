ccd430f2a3f282fb70691ebd6e37397132e59be7
Romano Livi <romano_livi@outlook.com>
2020-12-21 18:45:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,12 @@
 class Transfer
   attr_accessor :sender, :receiver, :status, :amount
   
-  def initialize(sender, receiver)
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
   
   
 end
